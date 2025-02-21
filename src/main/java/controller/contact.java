3ba004abecba621733ee0633c8810d550018d78a
Darshanvenky <141915302+Darshanvenky@users.noreply.github.com>
2025-02-21 14:49:47 +0530

    Update contact.java

--- src/main/java/controller/contact.java
+++ src/main/java/controller/contact.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.io.IOException;
@@ -16,10 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.Registration1;
 
-/**
- *
- * @author BSIL
- */
+
 @WebServlet(name = "contact", urlPatterns = {"/contact"})
 public class contact extends HttpServlet {
 
@@ -97,6 +90,6 @@ public class contact extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
