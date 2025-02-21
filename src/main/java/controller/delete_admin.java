a02532ed772386984bf483ac9a84c1cfa4a626ae
Darshanvenky <141915302+Darshanvenky@users.noreply.github.com>
2025-02-21 14:51:05 +0530

    Update delete_admin.java

--- src/main/java/controller/delete_admin.java
+++ src/main/java/controller/delete_admin.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.io.IOException;
@@ -60,6 +56,6 @@ public class delete_admin extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
