69aaa5d789d3d15cc789f48114b6e74e1887fb15
Darshanvenky <141915302+Darshanvenky@users.noreply.github.com>
2025-02-21 14:50:42 +0530

    Update delete.java

--- src/main/java/controller/delete.java
+++ src/main/java/controller/delete.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.io.IOException;
@@ -15,10 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.Registration1;
 
-/**
- *
- * @author BSIL
- */
+
 @WebServlet(name = "delete", urlPatterns = {"/delete"})
 public class delete extends HttpServlet {
 
@@ -55,9 +48,6 @@ public class delete extends HttpServlet {
 
     }
 
-
-
-// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
 /**
  * Handles the HTTP <code>GET</code> method.
  *
@@ -94,6 +84,6 @@ public class delete extends HttpServlet {
     @Override
         public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
